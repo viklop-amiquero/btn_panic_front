6e95b4a72754a8bef1508e16026ca5a3fa8958f2
viklop <viklop7025@gmail.com>
2025-03-14 12:42:08 -0500

    feat: validación de los campos de registro.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidationErrors,
+    Validators,
+} from '@angular/forms'
 
 @Component({
     selector: 'app-register-page',
@@ -12,14 +18,76 @@ export class RegisterPageComponent implements OnInit {
 
     constructor() {}
 
-    public registerForm: FormGroup = this._fb.group({
-        nombre: [''],
-        apellido: [''],
-        email: [''],
-        telefono: [''],
-    })
+    public registerForm: FormGroup = this._fb.group(
+        {
+            nombre: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern('^[A-Za-zÁÉÍÓÚáéíóúÑñ\\s]+$'),
+                ],
+            ],
+            apellido: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern('^[A-Za-zÁÉÍÓÚáéíóúÑñ\\s]+$'),
+                ],
+            ],
+            email: ['', [Validators.required, Validators.email]],
+            telefono: [
+                '',
+                [Validators.required, Validators.pattern('^[0-9]+$')],
+            ],
+            password: [
+                '',
+                [
+                    Validators.required,
+                    Validators.minLength(6),
+                    Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d).+$'),
+                ],
+            ],
+            password_confirmation: ['', [Validators.required]],
+        },
+        {
+            validators: this.isFieldOneEqualFieldTwo(
+                'password',
+                'password_confirmation'
+            ),
+        }
+    )
 
     ngOnInit() {}
+
+    public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+        return (formGroup: AbstractControl): ValidationErrors | null => {
+            const fieldValue1 = formGroup.get(field1)?.value
+            const fieldValue2 = formGroup.get(field2)?.value
+
+            if (fieldValue1 !== fieldValue2) {
+                console.log({
+                    condicional: true,
+                    field1: fieldValue1,
+                    field2: fieldValue2,
+                })
+
+                formGroup.get(field2)?.setErrors({ notEqual: true })
+
+                return { notEqual: true }
+            }
+
+            formGroup.get(field2)?.setErrors(null)
+
+            return null
+        }
+    }
+
+    isInvalidField(field: string): boolean | null {
+        return (
+            this.registerForm.controls[field].errors &&
+            this.registerForm.controls[field].touched
+        )
+    }
 
     submitForm() {}
 }
