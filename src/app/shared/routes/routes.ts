7fe3b3da7a8e57dde5a94ba9c0ec8f5120c1ab5e
viklop <viklop7025@gmail.com>
2025-04-01 18:02:58 -0500

    feat: añadir clase "active" cuando se encuentre en una ruta específica.

--- src/app/shared/routes/routes.ts
+++ src/app/shared/routes/routes.ts
@@ -18,7 +18,7 @@ export const RoutesName = {
     AUTH: { route: 'auth', name: '', icon: 'lock-closed-outline' },
     LOGIN: { route: 'login', name: '', icon: 'log-in-outline' },
     REGISTER: { route: 'register', name: '', icon: 'person-add-outline' },
-    INDEX: { route: 'index', name: 'Inicio', icon: 'home-outline' },
+    INDEX: { route: '', name: 'Inicio', icon: 'home-outline' },
     PROFILE: { route: 'profile', name: 'Mi cuenta', icon: 'person-outline' },
     REPORTS: {
         route: 'reports',
