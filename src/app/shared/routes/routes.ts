ee59318df00a9702cdd0128dcce71737044f1019
viklop <viklop7025@gmail.com>
2025-04-01 18:04:14 -0500

    fix: Eliminar comentarios.

--- src/app/shared/routes/routes.ts
+++ src/app/shared/routes/routes.ts
@@ -1,19 +1,3 @@
-// export const RoutesName = {
-//     AUTH: 'auth',
-//     LOGIN: 'login',
-//     REGISTER: 'register',
-//     INDEX: 'index',
-//     PROFILE: 'profile',
-//     REPORTS: 'reports',
-// }
-// export const RoutesName = {
-//     AUTH: { route: 'auth', name: '' },
-//     LOGIN: { route: 'login', name: '' },
-//     REGISTER: { route: 'register', name: '' },
-//     INDEX: { route: 'index', name: 'Inicio' },
-//     PROFILE: { route: 'profile', name: 'Mi cuenta' },
-//     REPORTS: { route: 'reports', name: 'Mi actividad' },
-// }
 export const RoutesName = {
     AUTH: { route: 'auth', name: '', icon: 'lock-closed-outline' },
     LOGIN: { route: 'login', name: '', icon: 'log-in-outline' },
