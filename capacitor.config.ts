287d7e66adf3153335f5868b80dd722ef4be9c67
viklop <viklop7025@gmail.com>
2025-04-09 12:16:17 -0500

    fix: Corregir mensaje de error de inputs.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'btn_panic_front',
-  webDir: 'www'
-};
+    appId: 'com.example.app',
+    appName: 'btn_panic_front',
+    webDir: 'www',
+}
 
-export default config;
+export default config
