9af2836bc333e39eed8d836b3b84545df92a26a0
viklop <viklop7025@gmail.com>
2025-04-10 09:44:32 -0500

    feat: Agregar splash  e ícono.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,25 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
     appId: 'com.example.app',
-    appName: 'btn_panic_front',
+    appName: 'Botón de pánico',
     webDir: 'www',
+    plugins: {
+        SplashScreen: {
+            launchShowDuration: 0,
+            launchAutoHide: true,
+            backgroundColor: '#9ae600',
+            androidSplashResourceName: 'splash',
+            androidScaleType: 'CENTER_CROP',
+            showSpinner: false,
+            androidSpinnerStyle: 'large',
+            iosSpinnerStyle: 'small',
+            spinnerColor: '#999999',
+            splashFullScreen: false,
+            splashImmersive: false,
+            layoutName: 'launch_screen',
+            useDialog: false,
+        },
+    },
 }
 
 export default config
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { SplashScreen } from '@capacitor/splash-screen'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-  standalone: false,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
+    standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+    constructor() {
+        this.showSplash()
+    }
+
+    async showSplash() {
+        await SplashScreen.show({
+            autoHide: true,
+            showDuration: 3000,
+        })
+    }
 }
